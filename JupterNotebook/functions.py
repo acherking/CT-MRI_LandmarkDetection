f091192402cf4f760f7ec0168702f19d4c237c2d
Shawn <wzxfate@gmail.com>
2022-11-10 13:35:48 +1100

    add tool function: load_mat_data

--- JupterNotebook/functions.py
+++ JupterNotebook/functions.py
@@ -3,6 +3,11 @@ import numpy
 from matplotlib import pyplot
 from matplotlib.patches import Circle
 
+from os import listdir
+from os.path import isfile, join
+import numpy as np
+import h5py
+
 
 def show_pts(volume, pts, pixel_space):
     y_row = numpy.arange(0.0, volume.shape[0] * pixel_space[0], pixel_space[0])
@@ -17,33 +22,55 @@ def show_pts(volume, pts, pixel_space):
     axs[0][0].set_title("LLSCC ant")
     axs[0][0].set_aspect('equal', 'datalim')
     axs[0][0].pcolormesh(x_column[:], y_row[:], volume[:, :, pts[0, 2]], cmap=pyplot.gray())
-    LLSCC_ant = Circle((pts[0, 0] * pixel_space[1], pts[0, 1] * pixel_space[0]), landmark_radius, facecolor='None',
+    llscc_ant = Circle((pts[0, 0] * pixel_space[1], pts[0, 1] * pixel_space[0]), landmark_radius, facecolor='None',
                        edgecolor='r', lw=1)
-    axs[0][0].add_patch(LLSCC_ant)
+    axs[0][0].add_patch(llscc_ant)
     axs[0][0].invert_yaxis()
 
     axs[0][1].set_title("LLSCC post")
     axs[0][1].set_aspect('equal', 'datalim')
     axs[0][1].pcolormesh(x_column[:], y_row[:], volume[:, :, pts[1, 2]], cmap=pyplot.gray())
-    LLSCC_post = Circle((pts[1, 0] * pixel_space[1], pts[1, 1] * pixel_space[0]), landmark_radius, facecolor='None',
+    llscc_post = Circle((pts[1, 0] * pixel_space[1], pts[1, 1] * pixel_space[0]), landmark_radius, facecolor='None',
                         edgecolor='r', lw=1)
-    axs[0][1].add_patch(LLSCC_post)
+    axs[0][1].add_patch(llscc_post)
     axs[0][1].invert_yaxis()
 
     axs[1][0].set_title("RLSCC ant")
     axs[1][0].set_aspect('equal', 'datalim')
     axs[1][0].pcolormesh(x_column[:], y_row[:], volume[:, :, pts[2, 2]], cmap=pyplot.gray())
-    RLSCC_ant = Circle((pts[2, 0] * pixel_space[1], pts[2, 1] * pixel_space[0]), landmark_radius, facecolor='None',
+    rlscc_ant = Circle((pts[2, 0] * pixel_space[1], pts[2, 1] * pixel_space[0]), landmark_radius, facecolor='None',
                        edgecolor='r', lw=1)
-    axs[1][0].add_patch(RLSCC_ant)
+    axs[1][0].add_patch(rlscc_ant)
     axs[1][0].invert_yaxis()
 
     axs[1][1].set_title("RLSCC post")
     axs[1][1].set_aspect('equal', 'datalim')
     axs[1][1].pcolormesh(x_column[:], y_row[:], volume[:, :, pts[3, 2]], cmap=pyplot.gray())
-    RLSCC_post = Circle((pts[3, 0] * pixel_space[1], pts[3, 1] * pixel_space[0]), landmark_radius, facecolor='None',
+    rlscc_post = Circle((pts[3, 0] * pixel_space[1], pts[3, 1] * pixel_space[0]), landmark_radius, facecolor='None',
                         edgecolor='r', lw=1)
-    axs[1][1].add_patch(RLSCC_post)
+    axs[1][1].add_patch(rlscc_post)
     axs[1][1].invert_yaxis()
 
     pyplot.show()
+
+
+def load_mat_data(volume_path, pts_path="None"):
+    load_mat_vol = []
+    load_mat_pts = []
+    if pts_path != "None":
+        file_volume = h5py.File(volume_path, 'r')
+        file_pts = h5py.File(pts_path, 'r')
+        load_mat_vol = file_volume.get('rescaled_aug_vol')
+        load_mat_pts = file_pts.get('rescaled_aug_pts')
+    else:
+        file_data = h5py.File(volume_path, 'r')
+        load_mat_vol = file_data.get('augVol')
+        load_mat_pts = file_data.get('augPts')
+
+    volume = np.array(load_mat_vol).T
+    pts = np.array(load_mat_pts).reshape(3, 4).T.astype(int)
+
+    # close file automatically: file_data or file_volume&file_pts
+
+    return volume, pts
+
