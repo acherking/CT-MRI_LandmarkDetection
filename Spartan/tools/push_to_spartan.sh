8d0af349f286589cb08be332eba0c52479707b0c
Shawn <wzxfate@gmail.com>
2022-11-15 13:09:25 +1100

    build push/pull tools to synchronize local and Spartan

---
+++ Spartan/tools/push_to_spartan.sh
@@ -0,0 +1,7 @@
+#!/bin/zsh
+
+base_path=/Users/achs/PhD/code/CT-MRI_LandmarkDetection/Spartan
+
+# set rsa pass in local ~/.ssh/id_rsa and remote ~/.ssh/authorized_keys(id_rsa.pub)
+# scp -p ../Spartan_GPU_training.py wezw@spartan.hpc.unimelb.edu.au:/data/gpfs/projects/punim1836/Modules
+rsync -tv -e ssh ${base_path}/*.py ${base_path}/*.slurm wezw@spartan.hpc.unimelb.edu.au:/data/gpfs/projects/punim1836/Modules
