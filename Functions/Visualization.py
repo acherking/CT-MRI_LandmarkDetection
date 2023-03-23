0bbc006911fbb5851b23fae7116551562456979d
Shawn <wzxfate@gmail.com>
2023-03-23 16:56:42 +1100

    crop: doing crop function (debug)

--- Functions/Visualization.py
+++ Functions/Visualization.py
@@ -48,3 +48,49 @@ def show_pts(volume, pts, pixel_space):
     axs[1][1].invert_yaxis()
 
     pyplot.show()
+
+
+def show_two_landmarks(left_volume, left_points, right_volume, right_points, pixel_space):
+    #
+    y_row = numpy.arange(0.0, left_volume.shape[0] * pixel_space[0], pixel_space[0])
+    x_column = numpy.arange(0.0, left_volume.shape[1] * pixel_space[1], pixel_space[1])
+    z_slice = numpy.arange(0.0, left_volume.shape[2] * pixel_space[2], pixel_space[2])
+
+    landmark_radius = left_volume.shape[1] * pixel_space[1] * 0.016
+
+    fig, axs = pyplot.subplots(2, 2, sharex=True)
+    fig.set_dpi(300)
+
+    axs[0][0].set_title("LLSCC ant")
+    axs[0][0].set_aspect('equal', 'datalim')
+    axs[0][0].pcolormesh(x_column[:], y_row[:], left_volume[:, :, int(left_points[0, 2])], cmap=pyplot.gray())
+    llscc_ant = Circle((left_points[0, 0] * pixel_space[1], left_points[0, 1] * pixel_space[0]), landmark_radius, facecolor='None',
+                       edgecolor='r', lw=1)
+    axs[0][0].add_patch(llscc_ant)
+    axs[0][0].invert_yaxis()
+
+    axs[0][1].set_title("LLSCC post")
+    axs[0][1].set_aspect('equal', 'datalim')
+    axs[0][1].pcolormesh(x_column[:], y_row[:], left_volume[:, :, int(left_points[1, 2])], cmap=pyplot.gray())
+    llscc_post = Circle((left_points[1, 0] * pixel_space[1], left_points[1, 1] * pixel_space[0]), landmark_radius, facecolor='None',
+                        edgecolor='r', lw=1)
+    axs[0][1].add_patch(llscc_post)
+    axs[0][1].invert_yaxis()
+
+    axs[1][0].set_title("RLSCC ant")
+    axs[1][0].set_aspect('equal', 'datalim')
+    axs[1][0].pcolormesh(x_column[:], y_row[:], right_volume[:, :, int(right_points[0, 2])], cmap=pyplot.gray())
+    rlscc_ant = Circle((right_points[0, 0] * pixel_space[1], right_points[0, 1] * pixel_space[0]), landmark_radius, facecolor='None',
+                       edgecolor='r', lw=1)
+    axs[1][0].add_patch(rlscc_ant)
+    axs[1][0].invert_yaxis()
+
+    axs[1][1].set_title("RLSCC post")
+    axs[1][1].set_aspect('equal', 'datalim')
+    axs[1][1].pcolormesh(x_column[:], y_row[:], right_volume[:, :, int(right_points[1, 2])], cmap=pyplot.gray())
+    rlscc_post = Circle((right_points[1, 0] * pixel_space[1], right_points[1, 1] * pixel_space[0]), landmark_radius, facecolor='None',
+                        edgecolor='r', lw=1)
+    axs[1][1].add_patch(rlscc_post)
+    axs[1][1].invert_yaxis()
+
+    pyplot.show()
