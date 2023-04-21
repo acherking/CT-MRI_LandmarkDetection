ef375876f6d8884100afedc43342f573688eb67b
Shawn <wzxfate@gmail.com>
2023-04-21 16:54:26 +1000

    step 1: evaluation --> bounding box distance function

--- Functions/MyCrop.py
+++ Functions/MyCrop.py
@@ -1,6 +1,57 @@
 import numpy as np
 
 
+def find_borders(volume_shape, anchor, crop_size):
+    """
+    crop_size: ((x_d, x_a), (y_d, y_a), (z_d, z_a))
+        x_d is the length from the centre of the given points to the descending direction of axis x, include the given points
+        x_a is the length from the centre of the given points to the ascending direction of axis x
+        ...
+    """
+    ((x_d, x_a), (y_d, y_a), (z_d, z_a)) = crop_size
+
+    # find the border on axis x
+    x_start = 0
+    x_idx_min = anchor[0] - (x_d - 1)
+    x_length = x_d + x_a
+    if x_idx_min < 0:
+        x_length = x_length - abs(x_idx_min)
+        x_start = abs(x_idx_min)
+        x_idx_min = 0
+    x_idx_max = anchor[0] + x_a
+    if x_idx_max > (volume_shape[0] - 1):
+        x_length = x_length - (x_idx_max - (volume_shape[0] - 1))
+        x_idx_max = volume_shape[0] - 1
+
+    # find the border on axis y
+    y_start = 0
+    y_idx_min = anchor[1] - (y_d - 1)
+    y_length = y_d + y_a
+    if y_idx_min < 0:
+        y_length = y_length - abs(y_idx_min)
+        y_start = abs(y_idx_min)
+        y_idx_min = 0
+    y_idx_max = anchor[1] + y_a
+    if y_idx_max > (volume_shape[1] - 1):
+        y_length = y_length - (y_idx_max - (volume_shape[1] - 1))
+        y_idx_max = volume_shape[1] - 1
+
+    # find the border on axis z
+    z_start = 0
+    z_idx_min = anchor[2] - (z_d - 1)
+    z_length = z_d + z_a
+    if z_idx_min < 0:
+        z_length = z_length - abs(z_idx_min)
+        z_start = abs(z_idx_min)
+        z_idx_min = 0
+    z_idx_max = anchor[2] + z_a
+    if z_idx_max > (volume_shape[2] - 1):
+        z_length = z_length - (z_idx_max - (volume_shape[2] - 1))
+        z_idx_max = volume_shape[2] - 1
+
+    return x_start, x_length, x_idx_min, y_start, y_length, y_idx_min, z_start, z_length, z_idx_min
+
+
 def crop(volume, points, crop_size):
     """
     crop_size: ((x_d, x_a), (y_d, y_a), (z_d, z_a))
@@ -11,57 +62,22 @@ def crop(volume, points, crop_size):
     fill_value = np.min(volume)
     ((x_d, x_a), (y_d, y_a), (z_d, z_a)) = crop_size
     # initialize the cropped_volume with the minimal value in the volume
-    cropped_volume = np.ones((x_d+x_a, y_d+y_a, z_d+z_a)) * fill_value
+    cropped_volume = np.ones((x_d + x_a, y_d + y_a, z_d + z_a)) * fill_value
 
     centre = np.average(points, axis=0).astype(int)
 
-    # find the border on axis x
-    x_start = 0
-    x_idx_min = centre[0] - (x_d - 1)
-    x_length = x_d + x_a
-    if x_idx_min < 0:
-        x_length = x_length - abs(x_idx_min)
-        x_start = abs(x_idx_min)
-        x_idx_min = 0
-    x_idx_max = centre[0] + x_a
-    if x_idx_max > (volume.shape[0]-1):
-        x_length = x_length - (x_idx_max - (volume.shape[0] - 1))
-        x_idx_max = volume.shape[0] - 1
-
-    # find the border on axis y
-    y_start = 0
-    y_idx_min = centre[1] - (y_d - 1)
-    y_length = y_d + y_a
-    if y_idx_min < 0:
-        y_length = y_length - abs(y_idx_min)
-        y_start = abs(y_idx_min)
-        y_idx_min = 0
-    y_idx_max = centre[1] + y_a
-    if y_idx_max > (volume.shape[1]-1):
-        y_length = y_length - (y_idx_max - (volume.shape[1] - 1))
-        y_idx_max = volume.shape[1] - 1
-
-    # find the border on axis z
-    z_start = 0
-    z_idx_min = centre[2] - (z_d - 1)
-    z_length = z_d + z_a
-    if z_idx_min < 0:
-        z_length = z_length - abs(z_idx_min)
-        z_start = abs(z_idx_min)
-        z_idx_min = 0
-    z_idx_max = centre[2] + z_a
-    if z_idx_max > (volume.shape[2]-1):
-        z_length = z_length - (z_idx_max - (volume.shape[2] - 1))
-        z_idx_max = volume.shape[2] - 1
+    x_start, x_length, x_idx_min, \
+        y_start, y_length, y_idx_min, \
+        z_start, z_length, z_idx_min = find_borders(volume.shape, centre, crop_size)
 
     # crop the volume
-    cropped_volume[x_start:(x_start+x_length), y_start:(y_start+y_length), z_start:(z_start+z_length)] = \
-        volume[x_idx_min:(x_idx_min+x_length), y_idx_min:(y_idx_min+y_length), z_idx_min:(z_idx_min+z_length)]
+    cropped_volume[x_start:(x_start + x_length), y_start:(y_start + y_length), z_start:(z_start + z_length)] = \
+        volume[x_idx_min:(x_idx_min + x_length), y_idx_min:(y_idx_min + y_length), z_idx_min:(z_idx_min + z_length)]
     # cropped length, used to relocate the cropped points & find the point from the original whole volume
     cropped_length = np.ones(points.shape) * np.array([x_idx_min, y_idx_min, z_idx_min]) - \
                      np.ones(points.shape) * np.array([x_start, y_start, z_start])
     # relocate the points
-    cropped_points =  points - cropped_length
+    cropped_points = points - cropped_length
 
     return cropped_volume, cropped_points, cropped_length
 
@@ -96,3 +112,33 @@ def flip_volume(volume, points):
     flip_p[:, [0]] = np.ones(flip_p[:, [0]].shape) * (volume_s[1] - 1) - flip_p[:, [0]]
 
     return flip_v, flip_p
+
+
+def distance_from_border(volume_shape, points, anchor, crop_size=((50, 50), (50, 50), (50, 50))):
+    """
+    crop_size: ((x_d, x_a), (y_d, y_a), (z_d, z_a))
+        x_d is the length from the centre of the given points to the descending direction of axis x, include the given points
+        x_a is the length from the centre of the given points to the ascending direction of axis x
+        ...
+    points: two 3D points (2*3)
+    :return
+    (2*2*3) --> (2 points, descending&ascending, x&y&z)
+    """
+
+    x_start, x_length, x_idx_min, \
+        y_start, y_length, y_idx_min, \
+        z_start, z_length, z_idx_min = find_borders(volume_shape, anchor, crop_size)
+
+    cropped_length = np.ones(points.shape) * np.array([x_idx_min, y_idx_min, z_idx_min]) - \
+                     np.ones(points.shape) * np.array([x_start, y_start, z_start])
+
+    # points' distances to the borders, to the descending direction (just like the coordinate in the cropped volume:))
+    points_d = points - cropped_length
+    points_a = np.ones(points.shape) * np.array([x_length, y_length, z_length]) - points_d
+
+    # concatenate the results
+    points_d = np.expand_dims(points_d, axis=1)
+    points_a = np.expand_dims(points_a, axis=1)
+    ret = np.concatenate((points_d, points_a), axis=1)
+
+    return ret
