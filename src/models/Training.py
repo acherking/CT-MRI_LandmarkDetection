977b8965d9c2f07647e6ec8bead1c369188b2d07
Shawn <wzxfate@gmail.com>
2024-06-04 16:59:11 +1000

    try to fix some issues

--- src/models/Training.py
+++ src/models/Training.py
@@ -39,9 +39,7 @@ def train_model(args_dict):
     train_dataset = tf.data.Dataset.from_tensor_slices(train_dataset)
     train_dataset = train_dataset.shuffle(buffer_size=train_num * 2, reshuffle_each_iteration=True).batch(batch_size)
 
-    val_num = val_dataset[0].shape[0]
-    val_dataset = tf.data.Dataset.from_tensor_slices(val_dataset)
-    val_dataset = val_dataset.shuffle(buffer_size=val_num * 2, reshuffle_each_iteration=True).batch(batch_size)
+    val_dataset = tf.data.Dataset.from_tensor_slices(val_dataset).batch(batch_size)
 
     test_dataset = tf.data.Dataset.from_tensor_slices(test_dataset).batch(batch_size)
 
@@ -53,13 +51,6 @@ def train_model(args_dict):
         print("y value: ", y_batch_train)
         print("res: ", res_batch_train)
         break
-
-    for step, (x_batch_val, y_batch_val, res_batch_val) in enumerate(val_dataset):
-        print("val_dataset, step: ", step)
-        print("x shape: ", x_batch_val.shape, type(x_batch_val))
-        print("y value: ", y_batch_val)
-        print("res: ", res_batch_val)
-        break
     print("*** *** *** *** *** ***")
 
     optimizer = loss_optimizer.optimizer_manager(args_dict)
@@ -68,18 +59,12 @@ def train_model(args_dict):
 
     # Instantiate a metric object
     train_metric = keras.metrics.Mean()
-    val_metric = keras.metrics.Mean()
-    test_metric = keras.metrics.Mean()
 
     model = models.model_manager(args_dict)
     model.summary()
 
     if args_dict.get("write_log", True):
         log.flush()
-
-    # record the training process, for visualizing purpose
-    # 0: training err MSE over epoch, 1: val err MSE
-    train_err_array = np.zeros((2, epochs))
 
     @tf.function
     def train_step(x, y, res):
@@ -93,31 +78,27 @@ def train_model(args_dict):
         return mse_res
 
     @tf.function
-    def val_step(x, y, res):
+    def test_step(x):
         y_pred = model(x, training=False)
-        mse_res = mse_res_fn(y, y_pred, res)
-        val_metric.update_state(mse_res)
-        return y_pred
-
-    @tf.function
-    def test_step(x, y, res):
-        y_pred = model(x, training=False)
-        mse_res = mse_res_fn(y, y_pred, res)
-        test_metric.update_state(mse_res)
         return y_pred
 
     def my_evaluate(eval_dataset):
-        # Run a test loop when meet the best val result.
+        # evaluate the val or test dataset
         for s, (x_batch_test, y_batch_test, res_batch_test) in enumerate(eval_dataset):
             if s == 0:
-                y_test_p = test_step(x_batch_test, y_batch_test, res_batch_test)
+                y_pred = test_step(x_batch_test)
+                y_true = y_batch_test
+                res = res_batch_test
             else:
-                y_test = test_step(x_batch_test, y_batch_test, res_batch_test)
-                y_test_p = np.concatenate((y_test_p, y_test), axis=0)
+                y_pred = np.concatenate((y_pred, test_step(x_batch_test, y_batch_test, res_batch_test)), axis=0)
+                y_true = np.concatenate((y_true, y_batch_test), axis=0)
+                res = np.concatenate((res, res_batch_test), axis=0)
+        # prepare the evaluation metrics
+        err_diff = y_true - y_pred
+        num_landmarks = err_diff.shape[1]
+        rep_res = tf.repeat(res, num_landmarks, axis=1)
 
-        eval_metric_result = test_metric.result()
-        test_metric.reset_states()
-        return eval_metric_result, y_test_p
+        return y_pred
 
     # Training loop
     for epoch in range(epochs):
@@ -136,19 +117,13 @@ def train_model(args_dict):
 
         # Display metrics at the end of each epoch.
         train_mse_res = train_metric.result()
-        train_err_array[0][epoch] = float(train_mse_res)
         print("Training (MSE Res) over epoch:   %.4f" % (float(train_mse_res),))
 
         # Reset the metric's state at the end of an epoch
         train_metric.reset_states()
 
         # Run a validation loop at the end of each epoch.
-        for step, (x_batch_val, y_batch_val, res_batch_val) in enumerate(val_dataset):
-            val_step(x_batch_val, y_batch_val, res_batch_val)
 
-        val_mse_res = val_metric.result()
-        train_err_array[1][epoch] = float(val_mse_res)
-        val_metric.reset_states()
 
         # Show best Val results
         if val_mse_res < min_val_mse_res:
@@ -169,8 +144,6 @@ def train_model(args_dict):
     test_mse_res, y_test_pred = my_evaluate(test_dataset)
     np.save(f"{save_dir}/final_Y_test", y_test_pred)
     print("Test (MSE with Res), final       %.3f" % (float(test_mse_res),))
-
-    np.save(f"{save_dir}/train_val_err_array", train_err_array)
 
     sys.stdout = orig_stdout
     if args_dict.get("write_log", True):
@@ -205,6 +178,7 @@ if __name__ == "__main__":
         "y_tag": "two_landmarks",  # "one_landmark_[1/2]", "two_landmarks", "mean_two_landmarks"
         "model_label_1": "",  # Cross validation, different parameter...
         "model_label_2": "",
+        "save_model": True,
     }
 
     train_model(args)
--- src/models/start_training.py
+++ src/models/start_training.py
@@ -2,7 +2,7 @@ import sys
 
 import Training
 
-args = {
+base_args = {
     # prepare Dataset
     "dataset_tag": "divided",  # "divided", "cropped"
     ## for divided dataset ##
@@ -29,91 +29,68 @@ args = {
     "y_tag": "two_landmarks",  # "one_landmark_[1/2]", "two_landmarks", "mean_two_landmarks"
     "model_label_1": "",  # Cross validation, different parameter...
     "model_label_2": "",
+    "save_model": True,
 }
 
 
-def train_straight_model(idx=-1):
+def train_straight_model():
     # record the args?
     update_args_dict_list = [
         ## Identical Voxel distance with MSE_res
-        {"model_name": "straight_model", "model_output_num": 1, "y_tag": "mean_two_landmarks", "learning_rate": 0.0001,
+        {"train_id": 0, "model_name": "straight_model", "model_output_num": 1, "y_tag": "mean_two_landmarks", "learning_rate": 0.0001,
          "model_label_1": "learning_rate", "model_label_2": "0.0001"},
-        {"model_name": "straight_model", "model_output_num": 1, "y_tag": "mean_two_landmarks", "learning_rate": 0.00005,
+        {"train_id": 1, "model_name": "straight_model", "model_output_num": 1, "y_tag": "mean_two_landmarks", "learning_rate": 0.00005,
          "model_label_1": "learning_rate", "model_label_2": "0.00005"},
         ## Variable Voxel distance with MSE_res
-        {"model_name": "straight_model", "model_output_num": 1, "y_tag": "mean_two_landmarks", "learning_rate": 0.0001,
+        {"train_id": 2, "model_name": "straight_model", "model_output_num": 1, "y_tag": "mean_two_landmarks", "learning_rate": 0.0001,
          "model_label_1": "learning_rate", "model_label_2": "0.0001", "dataset_label_1": "variable_voxel_distance"},
-        {"model_name": "straight_model", "model_output_num": 1, "y_tag": "mean_two_landmarks", "learning_rate": 0.00005,
-         "model_label_1": "learning_rate", "model_label_2": "0.00005", "dataset_label_1": "variable_voxel_distance"}
+        {"train_id": 3, "model_name": "straight_model", "model_output_num": 1, "y_tag": "mean_two_landmarks", "learning_rate": 0.00005,
+         "model_label_1": "learning_rate", "model_label_2": "0.00005", "dataset_label_1": "variable_voxel_distance"},
+        {"train_id": 4, "model_name": "straight_model", "model_output_num": 1, "y_tag": "mean_two_landmarks", "learning_rate": 0.0001,
+         "model_label_1": "learning_rate", "model_label_2": "0.0001", "loss_name": "MSE"},
     ]
-
-    if idx < 0:
-        for i in range(len(update_args_dict_list)):
-            args.update(update_args_dict_list[i])
-            Training.train_model(args)
-    else:
-        args.update(update_args_dict_list[idx])
-        Training.train_model(args)
-
-    Training.train_model(args)
+    return update_args_dict_list
 
 
-def train_unet_model(idx=-1):
+def train_unet_model():
     update_args_dict_list = [
-        {"model_name": "u_net", "model_output_num": 2, "y_tag": "two_landmarks", "model_label_1": "basic"}]
-
-    if idx < 0:
-        for i in range(len(update_args_dict_list)):
-            args.update(update_args_dict_list[i])
-            Training.train_model(args)
-    else:
-        args.update(update_args_dict_list[idx])
-        Training.train_model(args)
-
-    Training.train_model(args)
+        {"model_name": "u_net", "model_output_num": 2, "y_tag": "two_landmarks", "model_label_1": "basic"}
+    ]
+    return update_args_dict_list
 
 
-def train_unet_dsnt_model(idx=-1):
+def train_unet_dsnt_model():
     update_args_dict_list = [
         ## Identical Voxel distance with MSE_res
-        {"model_name": "u_net_dsnt", "model_output_num": 2, "y_tag": "two_landmarks", "learning_rate": 0.0001,
+        {"train_id": 0, "model_name": "u_net_dsnt", "model_output_num": 2, "y_tag": "two_landmarks", "learning_rate": 0.0001,
          "model_label_1": "learning_rate", "model_label_2": "0.0001"},
-        {"model_name": "u_net_dsnt", "model_output_num": 2, "y_tag": "two_landmarks", "learning_rate": 0.00005,
+        {"train_id": 1, "model_name": "u_net_dsnt", "model_output_num": 2, "y_tag": "two_landmarks", "learning_rate": 0.00005,
          "model_label_1": "learning_rate", "model_label_2": "0.00005"},
-        {"model_name": "u_net_dsnt", "model_output_num": 2, "y_tag": "two_landmarks", "learning_rate": 0.00001,
+        {"train_id": 2, "model_name": "u_net_dsnt", "model_output_num": 2, "y_tag": "two_landmarks", "learning_rate": 0.00001,
          "model_label_1": "learning_rate", "model_label_2": "0.00001"},
-        {"model_name": "u_net_dsnt", "model_output_num": 2, "y_tag": "two_landmarks", "learning_rate": 0.000005,
+        {"train_id": 3, "model_name": "u_net_dsnt", "model_output_num": 2, "y_tag": "two_landmarks", "learning_rate": 0.000005,
          "model_label_1": "learning_rate", "model_label_2": "0.000005"},
-        {"model_name": "u_net_dsnt", "model_output_num": 2, "y_tag": "two_landmarks", "learning_rate": 0.000001,
+        {"train_id": 4, "model_name": "u_net_dsnt", "model_output_num": 2, "y_tag": "two_landmarks", "learning_rate": 0.000001,
          "model_label_1": "learning_rate", "model_label_2": "0.000001"},
         ## Variable Voxel distance with MSE_res
-        {"model_name": "u_net_dsnt", "model_output_num": 2, "y_tag": "two_landmarks", "learning_rate": 0.0001,
+        {"train_id": 5, "model_name": "u_net_dsnt", "model_output_num": 2, "y_tag": "two_landmarks", "learning_rate": 0.0001,
          "model_label_1": "learning_rate", "model_label_2": "0.0001", "dataset_label_1": "variable_voxel_distance"},
-        {"model_name": "u_net_dsnt", "model_output_num": 2, "y_tag": "two_landmarks", "learning_rate": 0.00005,
+        {"train_id": 6, "model_name": "u_net_dsnt", "model_output_num": 2, "y_tag": "two_landmarks", "learning_rate": 0.00005,
          "model_label_1": "learning_rate", "model_label_2": "0.00005", "dataset_label_1": "variable_voxel_distance"},
-        {"model_name": "u_net_dsnt", "model_output_num": 2, "y_tag": "two_landmarks", "learning_rate": 0.00001,
+        {"train_id": 7, "model_name": "u_net_dsnt", "model_output_num": 2, "y_tag": "two_landmarks", "learning_rate": 0.00001,
          "model_label_1": "learning_rate", "model_label_2": "0.00001", "dataset_label_1": "variable_voxel_distance"},
-        {"model_name": "u_net_dsnt", "model_output_num": 2, "y_tag": "two_landmarks", "learning_rate": 0.000005,
+        {"train_id": 8, "model_name": "u_net_dsnt", "model_output_num": 2, "y_tag": "two_landmarks", "learning_rate": 0.000005,
          "model_label_1": "learning_rate", "model_label_2": "0.000005", "dataset_label_1": "variable_voxel_distance"},
-        {"model_name": "u_net_dsnt", "model_output_num": 2, "y_tag": "two_landmarks", "learning_rate": 0.000001,
+        {"train_id": 9, "model_name": "u_net_dsnt", "model_output_num": 2, "y_tag": "two_landmarks", "learning_rate": 0.000001,
          "model_label_1": "learning_rate", "model_label_2": "0.000001", "dataset_label_1": "variable_voxel_distance"},
         ## Identical Voxel distance with MSE
-        {"model_name": "u_net_dsnt", "model_output_num": 2, "y_tag": "two_landmarks", "learning_rate": 0.00005,
+        {"train_id": 10, "model_name": "u_net_dsnt", "model_output_num": 2, "y_tag": "two_landmarks", "learning_rate": 0.00005,
          "model_label_1": "learning_rate", "model_label_2": "0.00005", "loss_name": "MSE"}
     ]
-
-    if idx < 0:
-        for i in range(len(update_args_dict_list)):
-            args.update(update_args_dict_list[i])
-            Training.train_model(args)
-    else:
-        args.update(update_args_dict_list[idx])
-        Training.train_model(args)
-
-    Training.train_model(args)
+    return update_args_dict_list
 
 
-def train_covonly_dsnt_model(idx=-1):
+def train_covonly_dsnt_model():
     update_args_dict_list = [
         ## Identical Voxel distance with MSE_res
         {"model_name": "cov_only_dsnt", "model_output_num": 2, "y_tag": "two_landmarks", "learning_rate": 0.0001,
@@ -138,28 +115,32 @@ def train_covonly_dsnt_model(idx=-1):
         {"model_name": "cov_only_dsnt", "model_output_num": 2, "y_tag": "two_landmarks", "learning_rate": 0.000001,
          "model_label_1": "learning_rate_K5", "model_label_2": "0.000001", "dataset_label_1": "variable_voxel_distance"}
     ]
-
-    if idx < 0:
-        for i in range(len(update_args_dict_list)):
-            args.update(update_args_dict_list[i])
-            Training.train_model(args)
-    else:
-        args.update(update_args_dict_list[idx])
-        Training.train_model(args)
-
-    Training.train_model(args)
+    return update_args_dict_list
 
 
 if __name__ == "__main__":
     train_model_name = sys.argv[1]
     train_id = int(sys.argv[2])
+
+    print(f"Training model: [{train_model_name}] with id: [{train_id}]")
+
     if train_model_name == "straight_model":
-        train_straight_model(train_id)
+        args_list = train_straight_model()
     elif train_model_name == "u_net":
-        train_unet_model(train_id)
+        args_list = train_unet_model()
     elif train_model_name == "u_net_dsnt":
-        train_unet_dsnt_model(train_id)
+        args_list = train_unet_dsnt_model()
     elif train_model_name == "cov_only_dsnt":
-        train_covonly_dsnt_model(train_id)
+        args_list = train_covonly_dsnt_model()
     else:
         print("Unknown model name: ", train_model_name)
+
+    if train_id < 0:
+        for i in range(len(args_list)):
+            args = base_args.copy()
+            args.update(args_list[i])
+            Training.train_model(args)
+    else:
+        args = base_args.copy()
+        args.update(args_list[train_id])
+        Training.train_model(args)
